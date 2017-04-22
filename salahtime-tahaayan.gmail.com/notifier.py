335d6a7b8650df2af135b1713f0a1de6ca5c0cad
Muhammed Taha Ayan <tahaayan@gmail.com>
2017-04-22 16:45:37 +0300

    Delete notifier.py

--- salahtime-tahaayan.gmail.com/notifier.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-import gi
-gi.require_version("Notify", "0.7")
-from gi.repository import Notify
-Notify.init("Salah")
-Hello = Notify.Notification.new("Hatirla", "Hatirla", "dialog-information")
-Hello.show()
